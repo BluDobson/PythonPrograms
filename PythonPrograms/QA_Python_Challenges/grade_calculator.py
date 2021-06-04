e47cf3be7b3e2791d851977d187076e0e374a6b5
BluDobson <bradleydobson11@hotmail.co.uk>
2021-06-04 14:31:13 +0000

    added grade_calculator.py

---
+++ PythonPrograms/QA_Python_Challenges/grade_calculator.py
@@ -0,0 +1,16 @@
+math_mark = int(input("Enter the Maths Mark: "))
+chem_mark = int(input("Enter the Chemistry Mark: "))
+phys_mark = int(input("Enter the Physics Mark: "))
+mark = math_mark + chem_mark + phys_mark
+percent = (mark / 300)*100
+
+if percent >= 70:
+    print("A")
+elif percent >= 60 and percent < 70:
+    print("B")
+elif percent >= 50 and percent < 60:
+    print("C")
+elif percent >= 40 and percent < 50:
+    print("B")
+else:
+    print("You failed")
