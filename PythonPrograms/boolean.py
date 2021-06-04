bd0fcb057c9a7484b64bc46dcd4b01eea763c2cc
BluDobson <bradleydobson11@hotmail.co.uk>
2021-06-04 10:46:27 +0000

    added boolean.py

---
+++ PythonPrograms/boolean.py
@@ -0,0 +1,9 @@
+# Pet
+name = "Charlie"
+age = 2
+bark = True
+tweet = False
+
+print(f"My pet is called {name}, he is {age} years old")
+print(f"Statement: {name} barks. {bark}")
+print(f"Statement: {name} tweets. {tweet}")
