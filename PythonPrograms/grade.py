8dcfa3d817a13452385f40e96b06f884716d5e8e
BluDobson <bradleydobson11@hotmail.co.uk>
2021-06-04 11:48:40 +0000

    edit grade.py

--- PythonPrograms/grade.py
+++ PythonPrograms/grade.py
@@ -1,8 +1,8 @@
 mark = int(input("Enter your mark here: "))
 
-if markint >= 85:
+if mark >= 85:
     print("Distinction")
-elif markint >= 65:
+elif mark >= 65 and mark <= 84:
     print("Pass")
 else:
     print("Fail")
