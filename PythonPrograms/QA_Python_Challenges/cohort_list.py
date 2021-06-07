f24ab7e65cd4032fa69eba9e777860d6faed2f5b
BluDobson <bradleydobson11@hotmail.co.uk>
2021-06-07 15:14:43 +0000

    added chort_list.py

---
+++ PythonPrograms/QA_Python_Challenges/cohort_list.py
@@ -0,0 +1,7 @@
+team = ["Bradley", "Umayr", "Steven", "Tommy", "Adi", "Samuel", "Callum", "Ethan", "Artas", "Layek", "Mofopefoluwa", "Daniel", "Lisa", "Albert", "Mitchell", "Aaron", "Reema", "Henry"]
+print(team)
+team.append("Raji")
+print(team)
+print(team[4])
+name_count = team.count("Chris")
+print(name_count)
