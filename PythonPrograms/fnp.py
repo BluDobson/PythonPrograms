1f7b869fae286b00e4d947b86906f4a3f82432ee
BluDobson <bradleydobson11@hotmail.co.uk>
2021-06-07 09:38:19 +0000

    added fnp.py

---
+++ PythonPrograms/fnp.py
@@ -0,0 +1,7 @@
+def add_calc(number1, number2):
+    answer = number1 + number2
+    return answer
+
+added_number = add_calc(int(input("Enter a number: ")), int(input("Enter a number: ")))
+
+print(added_number)
