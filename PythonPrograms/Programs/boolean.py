e3aef64d163c3e8367dccf9c68044a6ffff4acbc
BluDobson <bradleydobson11@hotmail.co.uk>
2021-06-08 11:20:45 +0000

    edit boolean.py

--- PythonPrograms/Programs/boolean.py
+++ PythonPrograms/Programs/boolean.py
@@ -1,9 +1,7 @@
 # Pet
-name = "Charlie"
-age = 2
-bark = True
-tweet = False
+def pet(name, age, bark, tweet):
+        print(f"My pet is called {name}, he is {age} years old")
+        print(f"Statement: {name} barks. {bark}")
+        print(f"Statement: {name} tweets. {tweet}")
 
-print(f"My pet is called {name}, he is {age} years old")
-print(f"Statement: {name} barks. {bark}")
-print(f"Statement: {name} tweets. {tweet}")
+data = pet(input("Enter your pet's name: "), input("Enter your pet's age: "), input("Does your pet bark? True/False: "), input("Does your pet tweet? True/False: "))
