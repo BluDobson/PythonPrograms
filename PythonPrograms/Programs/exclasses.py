55d53f4657737e3a221aff3b67d174a666cefd17
BluDobson <bradleydobson11@hotmail.co.uk>
2021-06-09 10:31:07 +0000

    edited exclasses.py

--- PythonPrograms/Programs/exclasses.py
+++ PythonPrograms/Programs/exclasses.py
@@ -6,9 +6,11 @@ class Student:
         self.score1 = score1
         self.score2 = score2
         self.score3 = score3
+        print(f"Name: {self.name}")
+        print(f"Age: {self.age}")
     
     def avg_score(self):
-        return int(self.score1 + self.score2 + self.score3)/3
+        return round(int(self.score1 + self.score2 + self.score3)/3)
 
 Entry = Student(input("Enter your name here: "), input("Enter your age here: "), int(input("Enter the first test score: ")), int(input("Enter the first second score: ")), int(input("Enter the first third score: ")))
-print(Entry.avg_score())
+print(f"Your average test score is: {Entry.avg_score()}")
