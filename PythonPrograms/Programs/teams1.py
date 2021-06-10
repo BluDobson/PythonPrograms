879cee564d0175dc031c229ca680882c375bd94e
BluDobson <bradleydobson11@hotmail.co.uk>
2021-06-10 11:16:50 +0000

    edited teams1.py

--- PythonPrograms/Programs/teams1.py
+++ PythonPrograms/Programs/teams1.py
@@ -2,7 +2,7 @@ def write_teams():
     file = open("teams.txt", "w")
     teams = ["Team 1: Chelsea", "Team 2: Manchester United", "Team 3: Liverpool", "Team 4: Arsenal", "Team 5: Manchester City"]
     for i in teams:
-        file.write(f"{i} \n")
+        file.write(f"{i}\n")
     file.close()
 
 def read_teams():
